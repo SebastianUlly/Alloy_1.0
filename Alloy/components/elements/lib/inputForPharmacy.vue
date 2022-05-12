1dcf25f878b634accf75cbc0887e550e7c1d2723
baratoddavid <102577787+baratoddavid@users.noreply.github.com>
2022-05-12 17:45:54 +0200

    guten Abend

---
+++ Alloy/components/elements/lib/inputForPharmacy.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div :key="index" v-for="index of selectLimit">
+      <select
+        class="select"
+        @change="onChange()"
+        onchange = "$emit('myevent')"
+        name=""
+        v-model="selectedPharmacy[index - 1]"
+      >
+        <option :value="item.label" v-for="item of options" :key="item.id">
+          {{ item.label }}
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    elementId: {
+      type: String,
+      required: true,
+    },
+    selectLimit: {
+      type: Number,
+      required: true,
+    },
+    options: {
+      type: Array,
+      required: true,
+    },
+    value: {
+      type: Array,
+    },
+    modelValue: {
+      type: Array,
+    },
+  },
+
+  data() {
+    return {
+      originalValue: ["", "", "", "", "", "", ""],
+      selectedPharmacy: ["", "", "", "", "", "", ""],
+    };
+  },
+  methods: {
+    onChange() {
+      if (
+        JSON.stringify(this.selectedPharmacy) !==
+        JSON.stringify(this.originalValue)
+      ) {
+        const payload = {};
+      }
+    },
+  },
+  computed: {
+      inputVal:{
+          get(){
+              return this.modelValue
+          },
+          set(val){
+            this.$emit('update:modelValue', val)
+          }
+      }
+  },
+};
+</script>
--- Alloy/components/metadata/pharmacyGroup/pharmacyGroup.vue
+++ Alloy/components/metadata/pharmacyGroup/pharmacyGroup.vue
@@ -1,18 +1,53 @@
 <template>
-    <div>
-       <div :key="index" v-for="(index) of parameters.selectLimit">
-           <selectInput
-           :label="label"
-           :elementId="elementId"
-           :required="parameters.required"
-           
-           /> 
-           </div>
-    </div>
-
+  <div>
+    <!-- <div :key="index" v-for="index of parameters.selectLimit">
+      <select class="select" name="" v-model="selectedPharmacy[index-1]">
+        <option id="option" :value="items.label" v-for="items of files" :key="items.id">
+          
+          {{ items.label }}
+        </option>
+      </select>
+    </div> -->
+    <inputForPharmacy
+      :elementId="elementId"
+      :options="files"
+      :selectLimit="parameters.selectLimit"
+    />
+      <vue-json-pretty :data="selectedPharmacy" />
+  </div>
 </template>
 <script>
+import gql from "graphql-tag";
+
 export default {
+  mounted() {
+    this.getfile();
+  },
+  methods: {
+    getfile() {
+      this.$apollo
+        .query({
+          variables: {
+            schemaId: "961fe75d-2d0e-4ccb-8afd-cde072b37380",
+          },
+          query: gql`
+            query ($schemaId: String) {
+              fileBySchemaId(schemaId: $schemaId) {
+                id
+                label
+              }
+            }
+          `,
+        })
+        .then((data) => {
+          this.files = data.data.fileBySchemaId;
+          /* console.log(data); */
+        })
+        .catch((error) => {
+          console.log({ error });
+        });
+    },
+  },
   props: {
     label: {
       type: String,
@@ -26,12 +61,33 @@ export default {
       type: Object,
       required: false,
       default: null,
-      
     },
-    
   },
   data() {
-    return {};
+    return { files: [], selectedPharmacy:["","","","","","",""]};
+  },
+  watch: {
+    asd: {
+
+    },
   },
 };
 </script>
+
+<style scoped>
+select {
+  border: solid white 2px;
+  border-radius: 4px;
+  margin: 0 auto;
+  width: 100%;
+  color: white;
+}
+div {
+  margin: auto;
+  width: 100%;
+  padding: 0 15px;
+}
+.required {
+  color: red;
+}
+</style>
