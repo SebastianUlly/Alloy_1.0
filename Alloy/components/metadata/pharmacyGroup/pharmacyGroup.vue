a5bb648a67223e29eb56184a43ea0c636db58aa4
Sebastian Ully <s.ully@bocom.at>
2022-06-23 09:31:33 +0200

    bugfixes

--- Alloy/components/metadata/pharmacyGroup/pharmacyGroup.vue
+++ Alloy/components/metadata/pharmacyGroup/pharmacyGroup.vue
@@ -18,137 +18,140 @@
 import gql from "graphql-tag";
 import { mapGetters } from "vuex";
 export default {
-  props: {
-    label: {
-      type: String,
-      required: true
-    },
-    children: {
-      type: Array,
-      required: true
-    },
-    elementId: {
-      type: String,
-      required: true
-    },
-    parameters: {
-      type: Object,
-      required: false,
-      default: null
-    }
-  },
-  data() {
-    return {
-      files: [],
-      selectedPharmacy: ["", "", "", "", "", "", ""],
-      originalSelectedPharmacy: ["", "", "", "", "", "", ""],
-      labelColor: "white"
-    };
-  },
-  methods: {
-    getfile() {
-      this.$apollo
-        .query({
-          variables: {
-            schemaId: this.parameters.selectableSchema[0]
-          },
-          query: gql`
-            query($schemaId: String) {
-              fileBySchemaId(schemaId: $schemaId) {
-                id
-                label
-                data
-              }
-            }
-          `
-        })
-        .then(data => {
-          const temp = data.data.fileBySchemaId
-          this.files =
-          temp.map(
-            function (item, index, array) {
-              return {
-                label: item.data.find(elementData => elementData.elementId === '91f42e63-98b4-462b-bf65-58b416718cb0')?.data?.text,
-                id: item.id
-              }
-            }
-          )
-        })
-        .catch(error => {
-          console.log({ error });
-        });
-    },
-    callback(data) {
-      this.selectedPharmacy = data;
-      let payload;
-      if (
-        JSON.stringify(this.selectedPharmacy) !==
-        JSON.stringify(this.originalSelectedPharmacy)
-      ) {
-        this.labelColor = "red";
-        payload = {
-          elementId: this.elementId,
-          hasChanged: true
-        };
-      } else {
-        this.labelColor = "white";
-        payload = {
-          elementId: this.elementId,
-          hasChanged: false
-        };
-      }
-      this.sendData();
-      this.$store.commit("infoBox/setHasChangedPropertyOfElement", payload);
-    },
-    sendData() {
-      const valuesToSend = JSON.parse(JSON.stringify(this.selectedPharmacy));
-      const payload = {
-        elementId: this.elementId,
-        data: {
-          values: valuesToSend
-        }
-      };
-      this.$store.commit("file/setEnteredData", payload);
-    },
-    findData() {
-      if (this.fileData) {
-        for (let item of this.fileData) {
-          if (this.elementId === item.elementId) {
-            if (item.data) {
-              const valuesFromDatabase = JSON.parse(
-                JSON.stringify(item.data.values)
-              );
-              this.originalSelectedPharmacy = valuesFromDatabase;
-              this.selectedPharmacy = valuesFromDatabase;
-              } else {
-                this.originalSelectedPharmacy = [];
-                this.selectedPharmacy = [];
-              }
-          }
-        }
-      }
-    }
-  },
-  mounted() {
-    this.getfile();
-    const payload = {
-      elementId: this.elementId,
-      hasChanged: false
-    };
-    this.findData();
-    this.$store.commit("infoBox/addToHasChangedArray", payload);
-  },
-  computed: {
-    ...mapGetters({ fileData: "file/getFileData" })
-  },
-  watch: {
-    fileData: {
-      deep: true,
-      handler() {
-        this.findData();
-      }
-    }
-  }
+	props: {
+		label: {
+			type: String,
+			required: true,
+		},
+		children: {
+			type: Array,
+			required: true,
+		},
+		elementId: {
+			type: String,
+			required: true,
+		},
+		parameters: {
+			type: Object,
+			required: false,
+			default: null,
+		},
+	},
+	data() {
+		return {
+			files: [],
+			selectedPharmacy: ["", "", "", "", "", "", ""],
+			originalSelectedPharmacy: ["", "", "", "", "", "", ""],
+			labelColor: "white",
+		};
+	},
+	methods: {
+		getfile() {
+		this.$apollo
+			.query({
+			variables: {
+				schemaId: this.parameters.selectableSchema[0],
+			},
+			query: gql`
+				query ($schemaId: String) {
+				fileBySchemaId(schemaId: $schemaId) {
+					id
+					label
+					data
+				}
+				}
+			`,
+			})
+			.then((data) => {
+				const temp = data.data.fileBySchemaId;
+				this.files = temp.map(
+					function (item, index, array) {
+						return item.data.find(
+							(elementData) =>
+								elementData.elementId === "91f42e63-98b4-462b-bf65-58b416718cb0"
+							)?.data?.text;
+					}
+				);
+			})
+			.catch((error) => {
+				console.log({ error });
+			});
+		},
+		callback(data) {
+			this.selectedPharmacy = data;
+			let payload;
+			if (
+				JSON.stringify(this.selectedPharmacy) !==
+				JSON.stringify(this.originalSelectedPharmacy)
+			) {
+				this.labelColor = "red";
+				payload = {
+					elementId: this.elementId,
+					hasChanged: true,
+				};
+			} else {
+				this.labelColor = "white";
+				payload = {
+					elementId: this.elementId,
+					hasChanged: false,
+				};
+			}
+			this.sendData();
+			this.$store.commit("infoBox/setHasChangedPropertyOfElement", payload);
+		},
+		sendData() {
+			const valuesToSend = JSON.parse(JSON.stringify(this.selectedPharmacy));
+			const payload = {
+				elementId: this.elementId,
+				data: {
+					values: valuesToSend,
+				},
+			};
+			this.$store.commit("file/setEnteredData", payload);
+		},
+		findData() {
+			// checking if fileData is found
+			if (this.fileData) {
+				// getting the correct item withe the elementId
+				const item = this.fileData.find(
+					(item) => item.elementId === this.elementId
+				);
+				// checking if the item is found and has data
+				if (item?.data?.text) {
+					const valuesFromDatabase = JSON.parse(
+						JSON.stringify(item.data.text)
+					);
+					this.originalSelectedPharmacy = valuesFromDatabase;
+					this.selectedPharmacy = valuesFromDatabase;
+				} else {
+					this.originalSelectedPharmacy = [];
+					this.selectedPharmacy = [];
+				}
+			}
+		},
+	},
+	mounted() {
+			this.getfile();
+			const payload = {
+				elementId: this.elementId,
+				hasChanged: false,
+			};
+			this.findData();
+			this.$store.commit("infoBox/addToHasChangedArray", payload);
+	},
+	computed: {
+		...mapGetters({
+			fileData: "file/getFileData"
+		}),
+	},
+	watch: {
+		fileData: {
+			deep: true,
+			handler() {
+				this.findData();
+			},
+		},
+	},
 };
 </script>
 <style scoped>
