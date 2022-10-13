0a4143b8a75c4537ad0f97bb234e67d0a0de767e
baratoddavid <baratoddavid@gmail.com>
2022-10-13 12:14:05 +0200

    13.10 adding ClientOnly tag to the HTML and checking permissions at the rows

--- Alloy/components/frontEnd/projektListe.vue
+++ Alloy/components/frontEnd/projektListe.vue
@@ -1,95 +1,99 @@
 <template>
+	
 	<div class="container">
-		<div>
-			<!-- the top sectioin contains the new addProject button yearSelect and the SearchFIeld -->
-			<div class="topSection">
-				<!-- the button sets the popUp variable true and the popUp window will appear -->
-				<v-btn
-					v-if="checkPermissionIdsHere('a9c5c480-c9d9-4564-9e21-3c78c493f203')"
-					style="min-width:0"
-					class="addProject"
-					color="green"
-					@click="openNewProject(true)"
-				>
-					<!-- the icon with the file and + -->
-					<v-icon>
-						mdi-note-plus-outline
-					</v-icon>
-				</v-btn>
-				<!-- the search container contains the events of searchComponent and the yearSelector -->
-				<div class="searchContainer">
-					<selectYear class="selectYearComponent" @sendYear="captureMyYear" v-if="checkPermissionIdsHere('b77ac80c-39ea-4550-b04c-843ad07a3672')"/>
-					<search @sendValue="captureMySearchValue" v-if="checkPermissionIdsHere('b77ac80c-39ea-4550-b04c-843ad07a3672')"/>
+		<ClientOnly>
+			<div>
+				<!-- the top sectioin contains the new addProject button yearSelect and the SearchFIeld -->
+				<div class="topSection">
+					<!-- the button sets the popUp variable true and the popUp window will appear -->
+					<v-btn
+						v-if="checkPermissionIdsHere('a9c5c480-c9d9-4564-9e21-3c78c493f203')"
+						style="min-width:0"
+						class="addProject"
+						color="green"
+						@click="openNewProject(true)"
+					>
+						<!-- the icon with the file and + -->
+						<v-icon>
+							mdi-note-plus-outline
+						</v-icon>
+					</v-btn>
+					<!-- the search container contains the events of searchComponent and the yearSelector -->
+					<div class="searchContainer">
+						<selectYear class="selectYearComponent" @sendYear="captureMyYear" v-if="checkPermissionIdsHere('b77ac80c-39ea-4550-b04c-843ad07a3672')"/>
+						<search @sendValue="captureMySearchValue" v-if="checkPermissionIdsHere('b77ac80c-39ea-4550-b04c-843ad07a3672')"/>
+					</div>
+				</div>
+				<!-- the body div contains the data table and the popUp component if its opened -->
+				<div class="body">
+					<popUp
+						v-if="popUp && popUpSchema"
+						@closeNewProject="openNewProject($event)"
+						@saveSuccess="projectSaved"
+						:popUpSchema = "popUpSchema"
+						:clickedFile = "clickedFile"
+					/>
+					<!-- the headers must be filled up with the data from the schema -->
+					<!-- custom sorting, styleing settings can be made here -->
+					<v-data-table
+						v-if="headers && items && checkPermissionIdsHere('576f02ff-7e02-401c-872e-ab3a2972ca2b')"
+						:headers="headers"
+						:items="items"
+						:items-per-page="20"
+						:search="search"
+						class="table"
+						:header-props="{ sortIcon: null }"
+						:sort-by="'Nummer'"
+						:sort-desc="true"
+					>
+						<!-- this template contains the last column with the action buttons -->
+						<template 
+							#item.actions="{item}"
+							v-slot:item="props">
+								<!-- the icons div contains the action icons and buttons-->
+								<div class="icons">
+									<!-- the first icon is a timer watch with a small pencil at the corner, its open the set time popup-->
+									<button v-if="checkPermissionIdsHere('658c3149-1004-4ab9-9c1c-032390a61497')">
+										<v-icon>
+											mdi-timer-edit-outline
+										</v-icon>
+									</button>
+									<!-- the pencil icon is in the middle, it opens the edit project popUp -->
+									<button @click="openEditProject(item)" v-if="checkPermissionIdsHere('b4ac64b2-48ab-4509-84bf-d82e3196c1d1')">
+										<v-icon> 
+											mdi-pencil-outline
+										</v-icon>
+									</button>
+									<!-- copy to the clipboard icon is the last icon, if clicked, calls the copyToClipboard functioin and the snackbar function, copyes the text to the clipboard -->
+									<button
+										v-if="checkPermissionIdsHere('006f7d44-7c87-4292-990f-31385e56dfb8')"
+										@click=copyToClipboard(item) 
+										class="copyIcon"
+										:ref="item.id"
+										title="In die Zwischenablage kopieren!">
+										<!-- copy icon -->
+										<v-icon>
+											mdi-content-copy
+										</v-icon>
+									</button>
+								</div>
+						</template>
+					</v-data-table>
 				</div>
 			</div>
-			<!-- the body div contains the data table and the popUp component if its opened -->
-			<div class="body">
-				<popUp
-					v-if="popUp && popUpSchema"
-					@closeNewProject="openNewProject($event)"
-					@saveSuccess="projectSaved"
-					:popUpSchema = "popUpSchema"
-					:clickedFile = "clickedFile"
-				/>
-				<!-- the headers must be filled up with the data from the schema -->
-				<!-- custom sorting, styleing settings can be made here -->
-				<v-data-table
-					v-if="headers && items && checkPermissionIdsHere('576f02ff-7e02-401c-872e-ab3a2972ca2b')"
-					:headers="headers"
-					:items="items"
-					:items-per-page="20"
-					:search="search"
-					class="table"
-					:header-props="{ sortIcon: null }"
-					:sort-by="'Nummer'"
-					:sort-desc="true"
-				>
-					<!-- this template contains the last column with the action buttons -->
-					<template 
-						#item.actions="{item}"
-						v-slot:item="props">
-							<!-- the icons div contains the action icons and buttons-->
-							<div class="icons">
-								<!-- the first icon is a timer watch with a small pencil at the corner, its open the set time popup-->
-								<button v-if="checkPermissionIdsHere('658c3149-1004-4ab9-9c1c-032390a61497')">
-									<v-icon>
-										mdi-timer-edit-outline
-									</v-icon>
-								</button>
-								<!-- the pencil icon is in the middle, it opens the edit project popUp -->
-								<button @click="openEditProject(item)" v-if="checkPermissionIdsHere('b4ac64b2-48ab-4509-84bf-d82e3196c1d1')">
-									<v-icon> 
-										mdi-pencil-outline
-									</v-icon>
-								</button>
-								<!-- copy to the clipboard icon is the last icon, if clicked, calls the copyToClipboard functioin and the snackbar function, copyes the text to the clipboard -->
-								<button
-									v-if="checkPermissionIdsHere('006f7d44-7c87-4292-990f-31385e56dfb8')"
-									@click=copyToClipboard(item) 
-									class="copyIcon"
-									:ref="item.id"
-									title="In die Zwischenablage kopieren!">
-									<!-- copy icon -->
-									<v-icon>
-										mdi-content-copy
-									</v-icon>
-								</button>
-							</div>
-					</template>
-				</v-data-table>
-			</div>
-		</div>
-		<!-- the snackbar that appears on the top for 2,2s if the copy symbol clicked -->
-		<v-snackbar
-			:top="true"
-			height="10px"
-			v-model="snackbar"
-			color="green"
-			:timeout="timeoutSnackbar"
-		>
-			{{ text }}
-		</v-snackbar>
+			<!-- the snackbar that appears on the top for 2,2s if the copy symbol clicked -->
+			<v-snackbar
+				:top="true"
+				height="10px"
+				v-model="snackbar"
+				color="green"
+				:timeout="timeoutSnackbar"
+			>
+				{{ text }}
+			</v-snackbar>
+		</ClientOnly>
 	</div>	
+	
 </template>
 
 <script>
@@ -269,21 +273,34 @@ export default {
 					for (const item of [...this.querySchemaById.elements, ...this.querySchemaById.metadata?.metadata_elements]) {
 						//if the element ID is the same as we need, it will push the label and add the elementIdToFind to the label
 						if (item.elementId === elementIdToFind) {
-							
-							if(item.label === "Jahr" && this.checkPermissionIdsHere(item.permissionId)){
+							//checking if the row permissioinId needed or the user the permission has
+							if(item.permissionId && !this.checkPermissionIdsHere(item.permissionId)){
+								//if the permissinId exists but the user has no permission to see this row the continue will exacute
+								//it means that the for loop will jump to the next element
+								continue;
+							}
+							if(item.label === "Jahr"){
+								//pushing the row (object) with the following settings to the headers (array)
 								this.headers.push({
+									//the header name is the label of the item
 									text: item.label,
+									//position is center of this row
 									align:'center',
-									/*  width: "1%",  */
+									//it is not shortable by the Year
 									sortable: false,
+									//the value is the "key" that tells the table where to load the elements
+									//it should not contain spaces, capital letters, special characters
 									value: item.label.replace(/[^a-zA-Z ]/g, ""),
+									//giving the elementId to the headers object too
 									elementId: elementIdToFind,
+									// filtering the projects to get just the list with the desired year (value comes from the selectYear component)
 									filter: value => {
 										if (!this.year) return true
 										return value.toString().includes(this.year.toString())
 									}
 								})	
 							}
+							// the number is a special case where the width and the align must be set
 							else if(item.label === "Nummer"){
 								this.headers.push({
 									text: item.label,
@@ -294,16 +311,7 @@ export default {
 									elementId: elementIdToFind
 								})
 							}
-							/* else if(item.label === "Apo"){
-								this.headers.push({
-									text: item.label,
-									width:"1%",
-									align:'left',
-									sortable: true,
-									value: item.label.replace(/[^a-zA-Z ]/g, ""),
-									elementId: elementIdToFind
-								})
-							}   */
+							//every other case it will push with these default settings
 							else{
 								this.headers.push({
 									text: item.label,
@@ -317,6 +325,7 @@ export default {
 						}
 					}
 				}
+				//pushing the actions row to the headers
 				this.headers.push({
 					text: "Actions",
 					align:'center',
@@ -365,6 +374,7 @@ export default {
 				this.$store.commit('directory/setToStoreDirectory', directory)
 			}
 		},
+		//checks if the permissionId is in the permissions list and sends the permissionId to the checkPermissionId function
 		checkPermissionIdsHere (arg) {
 			if (this.permissions) {
 				return checkPermissionId(this.permissions, arg)
