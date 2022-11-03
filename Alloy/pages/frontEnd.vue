7be59ac2060b8d4b70c274d0592973ae7d3b3a01
baratoddavid <baratoddavid@gmail.com>
2022-11-03 09:43:42 +0100

    03.11

--- Alloy/pages/frontEnd.vue
+++ Alloy/pages/frontEnd.vue
@@ -13,32 +13,6 @@ Vue.prototype.$hostname = 'your variable name'
 export default {
     components: {
       	projektListe
-    },
-
-	created () {
-		this.getToken()
-		
-	},
-	
-	methods: {
-		getToken () {
-			
-			const token = Cookie.get('apollo-token')
-			// if a token is found
-			if (token) {
-				// extracting the encoded payload from the token
-				const base64Url = token.split('.')[1]
-				// replacing characters in the encoded payload
-				const base64 = base64Url.replace('-', '+').replace('_', '/')
-				// decoding and parsing the payload into a JSON-object
-				const payload = JSON.parse(window.atob(base64))
-
-				// console.log(payload)
-				
-				// coomiting the payload to the store
-				this.$store.commit('authentication/setTokenInfo', payload)
-			}
-		}
-	}
+    }
 };
 </script>
