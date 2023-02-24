e3ac32bc0682a4f3cfa8322528c5fa13ca7d5f7e
Sebastian Ully <s.ully@bocom.at>
2023-02-24 08:16:07 +0100

    zw

--- Alloy/components/elements/relationships/Relationship.vue
+++ Alloy/components/elements/relationships/Relationship.vue
@@ -1,5 +1,6 @@
 <template>
 	<div>
+		{{ directoryWithOnlyAllowedEntitiesAndParents }}
 		<!-- {{ relationships }} -->
 		<div class="tree" :id="elementId">
 			<!-- treeview for the selection of new relationships -->
@@ -121,7 +122,7 @@ export default {
 
 	methods: {
 		findSchemaIdsToRelatedElementIds () {
-			// console.log(this.schemaRelationships)
+			console.log(this.schemaRelationships)
 			for (const relationshipPossibility of this.schemaRelationships) {
 				// console.log(relationshipPossibility.relationship)
 				if (relationshipPossibility.relationship.some(item => item.elementId === this.elementId)) {
@@ -137,6 +138,7 @@ export default {
 
 		// function to get te directory for the selection of the relationships
 		filterDirectory () {
+			console.log(this.schemaIds)
 			// creating a new instance of a directory with only entities of the alowed schemas
 			const directory = new DirectoryOnlyWithAllowedSchemaIds(this.storeDirestory, this.schemaIds, this.fileValues.id)
 
